ec05279b833e94399d9c9cdf739a93fe2ef73dd9
morixhub <morix.dev@gmail.com>
2025-06-10 15:18:01 +0000

    Added xdelta3 + Adjusted some indentation

--- recipes-bsp/u-boot-imx/u-boot-imx/include/configs/gemini_env.h
+++ recipes-bsp/u-boot-imx/u-boot-imx/include/configs/gemini_env.h
@@ -17,87 +17,87 @@
  */
 
 #define GEMINI_ENV \
-	"bootargs_extra=vt.global_cursor_default=0 vt.cur_default=1 consoleblank=0 fbcon=logo-count:1 fbcon=logo-pos:center fbcon=nodefer\0" \
-	"pxeuuid="GEMINI_PXE_UUID"\0" \
-	"bootcmd=run bsp_bootcmd\0" \
-	"initrd=initram.img\0" \
-	"initrd_addr=0x43800000\0" \
-	"ramdisk_addr_r=0x43800000\0" \
-	"loadinitrd=echo Attempting load of initrd (${initrd})...; " \
-		"ext4load mmc ${mmcdev}:${mmcpart} ${initrd_addr} ${initrd}\0" \
-	"fitaddr=0x48000000\0" \
-	"fitimage=fit.img\0" \
-	"kernel_addr_r=0x48000000\0" \
-	"gemini_hw_detect=echo Gemini hardware detection report: gpioid=${"GEMINI_ENVVAR_BOARD_GPIOID"}, boardid=${"GEMINI_ENVVAR_BOARD_ID"}; " \
-		"if test -z \"${"GEMINI_ENVVAR_FORCED_BOARD_ID"}\"; then " \
-			"if test -n \"${"GEMINI_ENVVAR_BOARD_ID"}\" ; then " \
-				"gemini_fdt_file=${"GEMINI_ENVVAR_BOARD_ID"}.dtb; " \
-				"gemini_fit_conf=${"GEMINI_ENVVAR_BOARD_ID"}; " \
-			"else " \
-				"gemini_fdt_file=${fdtfile}; " \
-				"gemini_fit_conf=\"conf-default\"; " \
-			"fi; " \
-		"else " \
-			"gemini_fdt_file=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}.dtb; " \
-			"gemini_fit_conf=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}; " \
-		"fi; " \
-		"setenv "GEMINI_ENVVAR_HWDETECT_BOOTARGS" \""GEMINI_ENVVAR_BOARD_GPIOID"=${"GEMINI_ENVVAR_BOARD_GPIOID"} "GEMINI_ENVVAR_BOARD_ID"=${"GEMINI_ENVVAR_BOARD_ID"} "GEMINI_ENVVAR_FORCED_BOARD_ID"=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}\"\0" \
-	"loadfit=echo Attempting load of FIT image (${fitimage})...; " \
-		"ext4load mmc ${mmcdev}:${mmcpart} ${fitaddr} ${fitimage}\0" \
-	"fitboot=env set loadaddr ${fitaddr}; " \
-		"bootm ${fitaddr}#${gemini_fit_conf}\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd...; " \
-		"run gemini_hw_detect; " \
-		"run pxeboot; " \
-		"mmc dev ${mmcdev}; " \
-		"if mmc rescan; " \
-		"run mmcargs; " \
-		"then " \
-			"if run loadbootscript; " \
-			"then " \
-				"echo bootscript available... running...; " \
-				"run bootscript; " \
-			"else " \
-				"if run loadfit; " \
-				"then " \
-					"echo FIT image loaded successfully... booting...; " \
-					"run fitboot; " \
-				"else " \
-					"if run loadimage; " \
-					"then " \
-						"echo Image available... continue booting...; " \
-						"run mmcboot; " \
-					"else " \
-						"run pxeboot_nocheck; " \
-					"fi; " \
-				"fi; " \
-			"fi; " \
-		"fi\0" \
-	"loadbootscript=echo Attempting load of bootscript (${bsp_script})...; " \
-		"ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=echo Attempting load of image (${image})...; " \
-		"ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=echo Attempting load of DT (${gemini_fdt_file})...; " \
-		"ext4load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${gemini_fdt_file}\0" \
+    "bootargs_extra=vt.global_cursor_default=0 vt.cur_default=1 consoleblank=0 fbcon=logo-count:1 fbcon=logo-pos:center fbcon=nodefer\0" \
+    "pxeuuid="GEMINI_PXE_UUID"\0" \
+    "bootcmd=run bsp_bootcmd\0" \
+    "initrd=initram.img\0" \
+    "initrd_addr=0x43800000\0" \
+    "ramdisk_addr_r=0x43800000\0" \
+    "loadinitrd=echo Attempting load of initrd (${initrd})...; " \
+        "ext4load mmc ${mmcdev}:${mmcpart} ${initrd_addr} ${initrd}\0" \
+    "fitaddr=0x48000000\0" \
+    "fitimage=fit.img\0" \
+    "kernel_addr_r=0x48000000\0" \
+    "gemini_hw_detect=echo Gemini hardware detection report: gpioid=${"GEMINI_ENVVAR_BOARD_GPIOID"}, boardid=${"GEMINI_ENVVAR_BOARD_ID"}; " \
+        "if test -z \"${"GEMINI_ENVVAR_FORCED_BOARD_ID"}\"; then " \
+            "if test -n \"${"GEMINI_ENVVAR_BOARD_ID"}\" ; then " \
+                "gemini_fdt_file=${"GEMINI_ENVVAR_BOARD_ID"}.dtb; " \
+                "gemini_fit_conf=${"GEMINI_ENVVAR_BOARD_ID"}; " \
+            "else " \
+                "gemini_fdt_file=${fdtfile}; " \
+                "gemini_fit_conf=\"conf-default\"; " \
+            "fi; " \
+        "else " \
+            "gemini_fdt_file=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}.dtb; " \
+            "gemini_fit_conf=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}; " \
+        "fi; " \
+        "setenv "GEMINI_ENVVAR_HWDETECT_BOOTARGS" \""GEMINI_ENVVAR_BOARD_GPIOID"=${"GEMINI_ENVVAR_BOARD_GPIOID"} "GEMINI_ENVVAR_BOARD_ID"=${"GEMINI_ENVVAR_BOARD_ID"} "GEMINI_ENVVAR_FORCED_BOARD_ID"=${"GEMINI_ENVVAR_FORCED_BOARD_ID"}\"\0" \
+    "loadfit=echo Attempting load of FIT image (${fitimage})...; " \
+        "ext4load mmc ${mmcdev}:${mmcpart} ${fitaddr} ${fitimage}\0" \
+    "fitboot=env set loadaddr ${fitaddr}; " \
+        "bootm ${fitaddr}#${gemini_fit_conf}\0" \
+    "bsp_bootcmd=echo Running BSP bootcmd...; " \
+        "run gemini_hw_detect; " \
+        "run pxeboot; " \
+        "mmc dev ${mmcdev}; " \
+        "if mmc rescan; " \
+        "run mmcargs; " \
+        "then " \
+            "if run loadbootscript; " \
+            "then " \
+                "echo bootscript available... running...; " \
+                "run bootscript; " \
+            "else " \
+                "if run loadfit; " \
+                "then " \
+                    "echo FIT image loaded successfully... booting...; " \
+                    "run fitboot; " \
+                "else " \
+                    "if run loadimage; " \
+                    "then " \
+                        "echo Image available... continue booting...; " \
+                        "run mmcboot; " \
+                    "else " \
+                        "run pxeboot_nocheck; " \
+                    "fi; " \
+                "fi; " \
+            "fi; " \
+        "fi\0" \
+    "loadbootscript=echo Attempting load of bootscript (${bsp_script})...; " \
+        "ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
+    "bootscript=echo Running bootscript from mmc ...; " \
+        "source\0" \
+    "loadimage=echo Attempting load of image (${image})...; " \
+        "ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
+    "loadfdt=echo Attempting load of DT (${gemini_fdt_file})...; " \
+        "ext4load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${gemini_fdt_file}\0" \
     "mmcargs=echo Evaluating dual boot...; " \
-		"dbv_dual=\"\" ; " \
-		"dbv_dual_partitions=\"\" ; " \
-		"dbv_dual_files=\"\" ; " \
-		"if ext4ls mmc ${mmcdev}:3 ; then " \
-			"dbv_dual=1 ; " \
-			"dbv_dual_partitions=1 ; " \
-		"elif test -e mmc ${mmcdev}:1 fit.img.a || test -e mmc ${mmcdev}:1 Image.a ; then " \
-			"dbv_dual=1 ; " \
-			"dbv_dual_files=1 ; " \
-		"fi; " \
-		"if test -n \"$dbv_dual\" ; then " \
-			"if test -n \"$dbv_dual_partitions\" ; then " \
-				"echo DUAL BOOT MODE (partitions) ; " \
-			"else " \
-				"echo DUAL BOOT MODE (files) ; " \
-			"fi; " \
+        "dbv_dual=\"\" ; " \
+        "dbv_dual_partitions=\"\" ; " \
+        "dbv_dual_files=\"\" ; " \
+        "if ext4ls mmc ${mmcdev}:3 ; then " \
+            "dbv_dual=1 ; " \
+            "dbv_dual_partitions=1 ; " \
+        "elif test -e mmc ${mmcdev}:1 fit.img.a || test -e mmc ${mmcdev}:1 Image.a ; then " \
+            "dbv_dual=1 ; " \
+            "dbv_dual_files=1 ; " \
+        "fi; " \
+        "if test -n \"$dbv_dual\" ; then " \
+            "if test -n \"$dbv_dual_partitions\" ; then " \
+                "echo DUAL BOOT MODE (partitions) ; " \
+            "else " \
+                "echo DUAL BOOT MODE (files) ; " \
+            "fi; " \
             "if test -n \"${db_force_single}\" ; then " \
                 "echo FORCING SINGLE BOOT from half A... ; " \
                 "setenv db_active_half a ; " \
@@ -125,68 +125,68 @@
                     "fi; " \
                 "fi; " \
             "fi; " \
-			"saveenv ; " \
-			"if test -n \"$dbv_dual_partitions\" && test ${db_last_half} = b ; then " \
-				"setenv mmcpart 2 ; " \
-			"else " \
-				"setenv mmcpart 1 ; " \
-			"fi; " \
-			"if test -n \"$dbv_dual_files\" ; then " \
-				"setenv image ${image}.${db_active_half} ; " \
-				"setenv initrd ${initrd}.${db_active_half} ;  " \
-				"setenv fitimage ${fitimage}.${db_active_half} ; " \
-				"gemini_fdt_file=${gemini_fdt_file}.${db_active_half} ; " \
-			"fi; " \
-			"setenv dbargs db_active_half=${db_active_half} ; " \
-			"if test -n \"$dbv_dual_partitions\" ; then " \
-				"setenv dbargs ${dbargs} db_mode=partitions ; " \
-			"else " \
-				"setenv dbargs ${dbargs} db_mode=files ; " \
-			"fi; " \
+            "saveenv ; " \
+            "if test -n \"$dbv_dual_partitions\" && test ${db_last_half} = b ; then " \
+                "setenv mmcpart 2 ; " \
+            "else " \
+                "setenv mmcpart 1 ; " \
+            "fi; " \
+            "if test -n \"$dbv_dual_files\" ; then " \
+                "setenv image ${image}.${db_active_half} ; " \
+                "setenv initrd ${initrd}.${db_active_half} ;  " \
+                "setenv fitimage ${fitimage}.${db_active_half} ; " \
+                "gemini_fdt_file=${gemini_fdt_file}.${db_active_half} ; " \
+            "fi; " \
+            "setenv dbargs db_active_half=${db_active_half} ; " \
+            "if test -n \"$dbv_dual_partitions\" ; then " \
+                "setenv dbargs ${dbargs} db_mode=partitions ; " \
+            "else " \
+                "setenv dbargs ${dbargs} db_mode=files ; " \
+            "fi; " \
             "if test -n \"${db_force_single}\" ; then " \
                 "setenv dbargs ${dbargs} db_force_single ; " \
             "fi; " \
-		"else " \
-			"echo SINGLE BOOT MODE ; " \
-		"fi; " \
-		"setenv mmcroot /dev/mmcblk${mmcdev}p${mmcpart} rootwait rw ; " \
-		"setenv bootargs ${bootargs_extra} ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot} ${"GEMINI_ENVVAR_HWDETECT_BOOTARGS"} ${dbargs}\0" \
-	"mmcboot=echo Booting from mmc...; " \
- 		"if run loadfdt; " \
-		"then " \
-			"if run loadinitrd; " \
-			"then " \
-				"echo initrd loaded successfully... booting...; " \
-				"booti ${loadaddr} ${initrd_addr} ${fdt_addr_r}; " \
-			"else " \
-				"echo initrd not available... booting...; " \
-				"booti ${loadaddr} - ${fdt_addr_r}; " \
-			"fi; " \
-		"fi\0" \
-	"pxeboot=echo Booting from PXE...; " \
-		"if env exists pxe_disabled && itest $pxe_disabled == 1; then " \
-			"echo PXE disabled (by environment); " \
-		"elif test -e mmc ${mmcdev}:2 .sys/pxe.disabled || test -e mmc ${mmcdev}:3 .sys/pxe.disabled ; then " \
-			"echo PXE disabled (by file-system); " \
-		"else " \
-			"echo Attempting PXE...; " \
-			"dhcp; " \
-			"if pxe get; then " \
-				"echo PXE server found: attempting boot from PXE...; " \
-				"pxe boot; " \
-				"echo Boot from PXE server failed: attempting other boot sources...; " \
-			"else " \
-				"echo Cannot find PXE server: attempting other boot sources...; " \
-			"fi; " \
-		"fi\0" \
-	"pxeboot_nocheck=echo Booting from PXE...; " \
-		"dhcp; " \
-		"if pxe get; then " \
-			"echo PXE server found: attempting boot from PXE...; " \
-			"pxe boot; " \
-			"echo Boot from PXE server failed; " \
-		"else " \
-			"echo Cannot find PXE server: boot failed; " \
-		"fi\0"
+        "else " \
+            "echo SINGLE BOOT MODE ; " \
+        "fi; " \
+        "setenv mmcroot /dev/mmcblk${mmcdev}p${mmcpart} rootwait rw ; " \
+        "setenv bootargs ${bootargs_extra} ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot} ${"GEMINI_ENVVAR_HWDETECT_BOOTARGS"} ${dbargs}\0" \
+    "mmcboot=echo Booting from mmc...; " \
+         "if run loadfdt; " \
+        "then " \
+            "if run loadinitrd; " \
+            "then " \
+                "echo initrd loaded successfully... booting...; " \
+                "booti ${loadaddr} ${initrd_addr} ${fdt_addr_r}; " \
+            "else " \
+                "echo initrd not available... booting...; " \
+                "booti ${loadaddr} - ${fdt_addr_r}; " \
+            "fi; " \
+        "fi\0" \
+    "pxeboot=echo Booting from PXE...; " \
+        "if env exists pxe_disabled && itest $pxe_disabled == 1; then " \
+            "echo PXE disabled (by environment); " \
+        "elif test -e mmc ${mmcdev}:2 .sys/pxe.disabled || test -e mmc ${mmcdev}:3 .sys/pxe.disabled ; then " \
+            "echo PXE disabled (by file-system); " \
+        "else " \
+            "echo Attempting PXE...; " \
+            "dhcp; " \
+            "if pxe get; then " \
+                "echo PXE server found: attempting boot from PXE...; " \
+                "pxe boot; " \
+                "echo Boot from PXE server failed: attempting other boot sources...; " \
+            "else " \
+                "echo Cannot find PXE server: attempting other boot sources...; " \
+            "fi; " \
+        "fi\0" \
+    "pxeboot_nocheck=echo Booting from PXE...; " \
+        "dhcp; " \
+        "if pxe get; then " \
+            "echo PXE server found: attempting boot from PXE...; " \
+            "pxe boot; " \
+            "echo Boot from PXE server failed; " \
+        "else " \
+            "echo Cannot find PXE server: boot failed; " \
+        "fi\0"
 
 #endif
